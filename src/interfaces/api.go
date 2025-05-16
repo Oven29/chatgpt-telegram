20e83364a82d88ac161a5781f59debdfda15ae77
Oven29 <ovchinno.ivan@gmail.com>
2025-05-17 02:14:00 +0300

    Fix typos in api interface

--- src/interfaces/api.go
+++ src/interfaces/api.go
@@ -2,9 +2,11 @@ package interfaces
 
 import (
 	"context"
+
+	"github.com/m1guelpf/chatgpt-telegram/src/entities"
 )
 
 type PaymentProvider interface {
-	CreatePayment(ctx context.Context, req PaymentRequest) (*PaymentResponse, error)
-	VerifyPayment(ctx context.Context, req PaymentVerificationRequest) (*PaymentVerificationResponse, error)
+	CreatePayment(ctx context.Context, req entities.PaymentRequest) (*entities.PaymentResponse, error)
+	VerifyPayment(ctx context.Context, req entities.PaymentVerificationRequest) (*entities.PaymentVerificationResponse, error)
 }
